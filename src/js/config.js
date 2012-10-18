92205ee4749c6153a0c63cf3aa7839e77c51bac3
Joshua Henderson <joshhendo@outlook.com>
2012-10-19 00:46:28 +1100

    Attempted to make the drop down box functional. Didn't quite make it, I will need to speak with Andrew about the best way to do this.

--- src/js/config.js
+++ src/js/config.js
@@ -1,1 +1,11 @@
 var confWebSocketServer = "ws://192.168.0.108:3001/ks1205";
+
+var textures = new Array();
+
+textures[0] = new Array();
+textures[0][0] = "World";
+textures[0][1] = "textures/world.jpg";
+
+textures[1] = new Array();
+textures[1][0] = "Grid";
+textures[1][1] = "textures/grid.jpg";
